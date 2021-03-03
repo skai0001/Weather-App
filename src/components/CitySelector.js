36fc9601f51e277c1942d92d6abf89abb540ba45
Hasan Skaiky <hasanskaiky@Hasans-MacBook-Air.local>
2021-03-03 14:14:57 -0500

    added form onsubmit & onchange, added temperature for the weather card

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,10 +1,8 @@
 import React, {Fragment, useState} from 'react';
 import {Row, Col, FormControl, Button} from 'react-bootstrap';
-import Alert from './Alert';
 
 const CitySelector = ({onSearch}) => {
     const [city, setCity] = useState('');
-    const [alert, setAlert] = useState(false);
 
 
     const handleChange = (e) => {
@@ -13,17 +11,13 @@ const CitySelector = ({onSearch}) => {
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (city === '') {
-            console.log('inside check', alert);
-        }
-        console.log('inside submit', alert);
         onSearch(city)
     }
 
     console.log(alert);
 
     return (
-        /* <Fragment>
+         <Fragment>
              <Row>
                  <Col>
                      <h1>Search your city</h1>
@@ -45,19 +39,8 @@ const CitySelector = ({onSearch}) => {
                      <Button onClick={handleSubmit}>Check Weather</Button>
                  </Col>
              </Row>
-         </Fragment>*/
-        <div className="container">
-            {!alert ? <Alert alert={alert}/> : null}
+         </Fragment>
 
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label><h3>Search your city</h3></label>
-                    <input className="form-control" type="text" placeholder="Enter City" value={city}
-                           onChange={handleChange}/>
-                </div>
-                <button type="submit" className="btn btn-primary">Check Weather</button>
-            </form>
-        </div>
     );
 };
 
