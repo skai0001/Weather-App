7c02d145ed86ccca80e267fe04ddb3c27e22f017
Hasan Skaiky <hasanskaiky@Hasans-MacBook-Air.local>
2021-03-03 14:00:09 -0500

    fixing FE

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -1,35 +1,66 @@
 import React, {Fragment, useState} from 'react';
 import {Row, Col, FormControl, Button} from 'react-bootstrap';
+import Alert from './Alert';
 
 const CitySelector = ({onSearch}) => {
     const [city, setCity] = useState('');
+    const [alert, setAlert] = useState(false);
 
-   const onchange = (e) => {setCity(e.target.value)}
-   const onSubmit = () => {onSearch(city)}
+
+    const handleChange = (e) => {
+        setCity(e.target.value)
+
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (city === '') {
+            console.log('inside check', alert);
+        }
+        console.log('inside submit', alert);
+        onSearch(city)
+    }
+
+    console.log(alert);
 
     return (
-        <Fragment>
-            <Row>
-                <Col>
-                    <h1>Search your city</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={4}>
-                    <FormControl
-                        placeholder="Enter city"
-                        onChange={onchange}
-                        value={city}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Button onClick={onSubmit}>Check Weather</Button>
-                </Col>
-            </Row>
-        </Fragment>
+        /* <Fragment>
+             <Row>
+                 <Col>
+                     <h1>Search your city</h1>
+                 </Col>
+             </Row>
+             <Row>
+                 <Col xs={4}>
+                     <FormControl
+                         required
+                         placeholder="Enter city"
+                         type="text"
+                         onChange={handleChange}
+                         value={city}
+                     />
+                 </Col>
+             </Row>
+             <Row>
+                 <Col>
+                     <Button onClick={handleSubmit}>Check Weather</Button>
+                 </Col>
+             </Row>
+         </Fragment>*/
+        <div className="container">
+            {!alert ? <Alert alert={alert}/> : null}
+
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label><h3>Search your city</h3></label>
+                    <input className="form-control" type="text" placeholder="Enter City" value={city}
+                           onChange={handleChange}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Check Weather</button>
+            </form>
+        </div>
     );
 };
 
 export default CitySelector;
+
+
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import {Card} from 'react-bootstrap';
 
-const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
+const WeatherCard = ({dt, temp, temp_min, temp_max, main, icon}) => {
     // create a date object with Date class constructor
     const date = new Date(dt);
+    console.log(date.toTimeString());
+    const temperature = Math.round(temp);
+
     return (
-        <Card>
-            <Card.Img
-                variant="top"
-                // get the src from example url and pass the icon prop for icon code
-                src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-            />
-            <Card.Body>
-                <Card.Title>{main}</Card.Title>
-                {/*  datetime is received in milliseconds, let's turn into local date time */}
-                <p>
-                    {date.toLocaleDateString()} - {date.toLocaleTimeString()}
-                </p>
-                {/* minimum temperature */}
-                <p>Min: {temp_min}</p>
-                {/* maximum temperature */}
-                <p>Max: {temp_max}</p>
-            </Card.Body>
-        </Card>
+        <>
+
+            <Card>
+                <Card.Img
+                    variant="top"
+                    // get the src from example url and pass the icon prop for icon code
+                    src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+                />
+                <Card.Body>
+                    <Card.Title>{main}</Card.Title>
+                    {/*  datetime is received in milliseconds, let's turn into local date time */}
+                    <p>{temperature}°C</p>
+                    <p>
+                        {date.toDateString()} - {date.toLocaleTimeString()}
+                    </p>
+                    {/* minimum temperature */}
+                    <p>Min: {temp_min}</p>
+                    {/* maximum temperature */}
+                    <p>Max: {temp_max}</p>
+                </Card.Body>
+            </Card>
+        </>
     );
 };
 export default WeatherCard;
